b79da93354ceec6c93c24b283525057a61b2fa18
Alexander Nookinov <anookinov@gmail.com>
2021-10-16 09:57:47 +0700

    add double test and refactor avec macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ macro_rules! avec {
     () => {
         Vec::new()
     };
-    ($element:expr) => {{
+    ($($element:expr),+) => {{
         let mut vs = Vec::new();
-        vs.push($element);
+        $(vs.push($element);)*
         vs
     }};
 }
@@ -22,4 +22,13 @@ fn single() {
     assert!(!x.is_empty());
     assert_eq!(x.len(), 1);
     assert_eq!(x[0], 42);
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec![42, 43];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 43);
 }
